cb63b7f49ad23a0c0e036252a32ad686d51bd2db
RAVELOTAHIANA <9691748+Liivasoa@users.noreply.github.com>
2026-02-23 11:34:24 +0100

    [Conf][All] Init the app with springboot

---
+++ src/test/java/mg/msys/abde_back/AbdeApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.msys.abde_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
