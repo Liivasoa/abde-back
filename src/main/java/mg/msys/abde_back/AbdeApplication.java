964965d9c7f846eaca24080c070c5ad363f66edc
RAVELOTAHIANA <9691748+Liivasoa@users.noreply.github.com>
2026-02-23 17:56:32 +0100

    [Test] Retablish pipeline

--- src/main/java/mg/msys/abde_back/AbdeApplication.java
+++ src/main/java/mg/msys/abde_back/AbdeApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AbdeApplication {
 
 	public static void main(String[] args) {
-		SpringApplicatio.run(AbdeApplication.class, args);
+		SpringApplication.run(AbdeApplication.class, args);
 	}
 
 }
