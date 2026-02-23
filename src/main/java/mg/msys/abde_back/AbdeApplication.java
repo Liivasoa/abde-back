17167c626cd41ea19bd3ea8ec8feaca161f7af9a
RAVELOTAHIANA <9691748+Liivasoa@users.noreply.github.com>
2026-02-23 17:53:44 +0100

    [Test] Test pipeline fail

--- src/main/java/mg/msys/abde_back/AbdeApplication.java
+++ src/main/java/mg/msys/abde_back/AbdeApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AbdeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AbdeApplication.class, args);
+		SpringApplicatio.run(AbdeApplication.class, args);
 	}
 
 }
