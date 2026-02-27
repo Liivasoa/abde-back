5ea250ba050c2f1295511355bcb1f837ab5161c6
RAVELOTAHIANA <9691748+Liivasoa@users.noreply.github.com>
2026-02-27 16:08:57 +0100

    [Test][Adapter] Lets create an abstract class to be extended  by all addapter test to ensure that all adapter test will be executed only one test container

---
+++ src/test/java/mg/msys/abde_back/adapter/out/repository/AbstractJpaRepositoryTest.java
@@ -0,0 +1,31 @@
+package mg.msys.abde_back.adapter.out.repository;
+
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@ActiveProfiles("test")
+@DataJpaTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public abstract class AbstractJpaRepositoryTest {
+
+    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:18.1-alpine")
+            .withDatabaseName("abde_test")
+            .withUsername("postgres")
+            .withPassword("abde_test")
+            .withReuse(true);
+
+    static {
+        postgreSQLContainer.start();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
+}
