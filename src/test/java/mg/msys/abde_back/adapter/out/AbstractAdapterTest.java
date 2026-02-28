36817e95b186a5c6048dfcb30a21ed007a5e06d2
RAVELOTAHIANA <9691748+Liivasoa@users.noreply.github.com>
2026-02-28 22:50:26 +0100

    [Refacto][Adapter] Use @ServiceConnection on AbstratcAdapterTest

--- src/test/java/mg/msys/abde_back/adapter/out/AbstractAdapterTest.java
+++ src/test/java/mg/msys/abde_back/adapter/out/AbstractAdapterTest.java
@@ -1,31 +1,20 @@
-package mg.msys.abde_back.adapter.out.repository;
+package mg.msys.abde_back.adapter.out;
 
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 
 @ActiveProfiles("test")
 @DataJpaTest
+@Transactional
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public abstract class AbstractAdapterTest {
 
-    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:18.1-alpine")
-            .withDatabaseName("abde_test")
-            .withUsername("postgres")
-            .withPassword("abde_test")
-            .withReuse(true);
-
-    static {
-        postgreSQLContainer.start();
-    }
-
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
-        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
-        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
-    }
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:18.1-alpine");
 }
