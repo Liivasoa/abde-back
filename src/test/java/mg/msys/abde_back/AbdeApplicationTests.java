3199339c239622c58fa46fc73752678888310568
RAVELOTAHIANA <9691748+Liivasoa@users.noreply.github.com>
2026-02-28 22:42:17 +0100

    [Test][All] Use test container on application context test

--- src/test/java/mg/msys/abde_back/AbdeApplicationTests.java
+++ src/test/java/mg/msys/abde_back/AbdeApplicationTests.java
@@ -2,9 +2,18 @@ package mg.msys.abde_back;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AbdeApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:18.1-alpine");
 
 	@Test
 	void contextLoads() {
