d9f49c94af4ecd8ae7f26183e3a16d42b4ef6f3e
Liivasoa <liva.ravelotahiana@gmail.com>
2026-07-10 17:12:11 +0200

    Delete batch

--- src/main/java/mg/msys/abde_back/batch/entity/AuthorAggregation.java
+++
@@ -1,7 +0,0 @@
-package mg.msys.abde_back.batch.entity;
-
-import java.util.Map;
-import java.util.Set;
-
-public record AuthorAggregation(Map<String, Author> uniqueAuthors, Set<BookAuthorLink> links) {
-}
