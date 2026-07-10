8792169f66af1666db993933d6a260a5eed46a5c
Liivasoa <liva.ravelotahiana@gmail.com>
2026-07-10 17:12:11 +0200

    Delete batch

--- src/main/java/mg/msys/abde_back/batch/entity/Author.java
+++
@@ -1,4 +0,0 @@
-package mg.msys.abde_back.batch.entity;
-
-public record Author(String lastName, String firstNames, Integer birthYear, Integer deathYear, String normalizedKey) {
-}
--- src/main/java/mg/msys/abde_back/batch/entity/BookAuthorLink.java
+++
@@ -1,4 +0,0 @@
-package mg.msys.abde_back.batch.entity;
-
-public record BookAuthorLink(Long bookId, String authorKey) {
-}
